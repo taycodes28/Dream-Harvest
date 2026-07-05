86acad54d275e84181c12f45c81ec5ee10ab758a
taycodes28 <taymonayc@gmail.com>
2026-07-05 19:07:37 -0400

    I made my player walk(animated)

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,41 +1,33 @@
 extends NodeState
-@export var player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
 
-var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	if Input.is_action_pressed("walk_left"):
-		direction = Vector2.LEFT
-	elif Input.is_action_pressed("walk_right"):
-		direction = Vector2.RIGHT
-	elif Input.is_action_pressed("walk_up"):
-		direction = Vector2.UP
-	elif Input.is_action_pressed("walk_down"):
-		direction = Vector2.DOWN
-	else:
-		direction = Vector2.ZERO
-		
-	if direction == Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
-	elif direction == Vector2.DOWN:
+	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
 	else:
 		animated_sprite_2d.play("idle_front")
 	
-		
-
 
 func _on_next_transitions() -> void:
-	pass
+	GameInputEvents.movement_input()
+	
+	if GameInputEvents.is_movement_input():
+		transition.emit("Walk")
+		
+	if player.current_tool == DataTypes.Tools.AxeWood:
+		transition.emit("Chopping")
 
 
 func _on_enter() -> void:
@@ -43,4 +35,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
---
+++ scripts/globals/data_types.gd
@@ -0,0 +1,9 @@
+class_name DataTypes
+enum Tools {
+	None,
+	AxeWood,
+	TillGround,
+	WaterCrops,
+	PlantCorn,
+	PlantTomato
+}
