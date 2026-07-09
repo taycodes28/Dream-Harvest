2cc2b717bcf4817305741caba996755fc59c4fd1
taycodes28 <taymonayc@gmail.com>
2026-07-09 16:13:58 -0400

    I added a small house and a medium house

---
+++ scenes/components/hurt_component.gd
@@ -0,0 +1,9 @@
+class_name HurtComponent
+extends Area2D
+
+@export var tool : DataTypes.Tools = DataTypes.Tools.None
+
+signal on_hurt
+
+func _on_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
