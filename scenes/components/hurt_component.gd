116b71468fa4f71d45a67c62adc599509dffa867
taycodes28 <taymonayc@gmail.com>
2026-07-14 15:13:31 -0400

    I created choppable trees

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -3,7 +3,10 @@ extends Area2D
 
 @export var tool : DataTypes.Tools = DataTypes.Tools.None
 
-signal on_hurt
+signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
-	pass # Replace with function body.
+	var hit_component = area as HitComponent
+
+	if tool == hit_component.current_tool:
+		hurt.emit(hit_component.hit_damage)
