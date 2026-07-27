869b38f414300552cd670f5b5dff154b458f888b
taycodes28 <taymonayc@gmail.com>
2026-07-27 17:00:44 -0400

    I made an inventory. When I pick up collectables it counts it!

---
+++ scripts/globals/inventory_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var inventory: Dictionary = Dictionary()
+
+signal inventory_changed
+
+func add_collectable(collectable_name: String) -> void:
+	inventory.get_or_add(collectable_name)
+	
+	if inventory[collectable_name] == null:
+		inventory[collectable_name] = 1
+	else:
+		inventory[collectable_name] += 1
+		
+		print(inventory)
+	inventory_changed.emit()
+		
