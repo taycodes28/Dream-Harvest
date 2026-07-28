9d78dce11c13e1b41d10da67251b3bd0d910002f
taycodes28 <taymonayc@gmail.com>
2026-07-28 18:48:39 -0400

    I created plant and corn game objects. My corn and tomatos can be grown and picked up

--- scripts/globals/data_types.gd
+++ scripts/globals/data_types.gd
@@ -1,4 +1,5 @@
 class_name DataTypes
+
 enum Tools {
 	None,
 	AxeWood,
@@ -6,4 +7,13 @@ enum Tools {
 	WaterCrops,
 	PlantCorn,
 	PlantTomato
+}
+
+enum GrowthStates {
+	Seed,
+	Germination,
+	Vegetative,
+	Reproduction,
+	Maturity,
+	Harvesting
 }
