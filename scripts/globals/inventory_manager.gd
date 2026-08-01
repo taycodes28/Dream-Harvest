9545661c654fbe0fd1f7733b23f1a8101686c805
taycodes28 <taymonayc@gmail.com>
2026-08-01 12:26:44 -0400

    I created an interactable chest that deducts items from the inventory and rewards you

--- scripts/globals/inventory_manager.gd
+++ scripts/globals/inventory_manager.gd
@@ -14,4 +14,12 @@ func add_collectable(collectable_name: String) -> void:
 		
 		print(inventory)
 	inventory_changed.emit()
+	
+func remove_collectable(collectable_name: String) -> void:
+	if inventory[collectable_name] == null:
+		inventory[collectable_name] = 0
+	else:
+		if inventory[collectable_name] > 0:
+			inventory[collectable_name] -= 1
 		
+	inventory_changed.emit()	
